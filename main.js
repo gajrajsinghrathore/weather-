9261948874ad2a050e7eb33ed3c26e59c5b51d63
gajraj singh <154729617+gajrajsinghrathore@users.noreply.github.com>
2024-08-31 21:02:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function GetInfo() {
 
     var newName = document.getElementById("cityInput");
     var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--";
+    cityName.innerHTML = "["+newName.value+"]";
 
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=9bb1d694a07e76ceda1272efc3df68f7')
 .then(response => response.json())
